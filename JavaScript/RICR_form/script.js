9924b7d99653189dc595a7de2cf79f9ce6c1028d
tanishthayadav12 <tanishthayadav12@gmail.com>
2025-11-21 11:06:15 +0530

    RICR Form

--- JavaScript/RICR_form/script.js
+++ JavaScript/RICR_form/script.js
@@ -6,22 +6,15 @@ function submitForm() {
   const email = document.getElementById("email").value;
   const qual = document.getElementById("qualification").value;
   const college = document.getElementById("collegeSchool").value;
-  const year = document.getElementById("year").value;
   const branch = document.getElementById("branch").value;
-  const source = document.getElementById("source").value;
-  const execName = document.getElementById("executive").value;
-  const msg = document.getElementById("message").value;
+
 
   console.log("Name:", name);
   console.log("Phone:", phone);
   console.log("Email:", email);
   console.log("Qualification:", qual);
   console.log("College:", college);
-  console.log("Year:", year);
   console.log("Branch:", branch);
-  console.log("Source:", source);
-  console.log("Executive:", execName);
-  console.log("Message:", msg);
 
   alert("Form submitted successfully!");
 
@@ -30,9 +23,6 @@ function submitForm() {
   document.getElementById("email").value = " ";
   document.getElementById("qualification").value = " ";
   document.getElementById("collegeSchool").value = " ";
-  document.getElementById("year").value = " ";
   document.getElementById("branch").value = " ";
-  document.getElementById("source").value = " ";
-  document.getElementById("executive").value = " ";
-  document.getElementById("message").value = "";
+
 }
