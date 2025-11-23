54b5a7cdb451bdde19b3610934d4728ef86703c5
tanishthayadav12 <tanishthayadav12@gmail.com>
2025-11-23 16:22:24 +0530

    Calculator

--- JavaScript/calculator/script.js
+++ JavaScript/calculator/script.js
@@ -1,11 +1,17 @@
-let display = document.getElementById("display");
-
-function button(value) {
-  display.value = display.value + value;
+function bt(char) {
+  if (char === "=") {
+    try {
+      document.getElementById("display").value = eval(
+        document.getElementById("display").value
+      );
+    } catch (error) {
+      alert("Invalid Expression");
+      document.getElementById("display").value = " ";
+    }
+  } else if (char === "clear") {
+    document.getElementById("display").value = " ";
+  } else {
+    document.getElementById("display").value =
+      document.getElementById("display").value + char;
+  }
 }
-
-function clear() {
-  display.value = "";
-}
-
-
