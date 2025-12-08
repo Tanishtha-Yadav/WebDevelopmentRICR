e519ec84563d42719862b01446044e2ecf7fd79c
tanishthayadav12 <tanishthayadav12@gmail.com>
2025-12-09 01:34:32 +0530

    Password Manager

---
+++ JavaScript/PasswordManager/script.js
@@ -0,0 +1,48 @@
+function AddData() {
+  const Site = document.getElementById("siteName").value.trim();
+  const UN = document.getElementById("userName").value.trim();
+  const PS = document.getElementById("password").value.trim();
+
+  const DataPacket = {
+    webSite: Site,
+    userName: UN,
+    password: PS,
+  };
+  console.log(DataPacket);
+
+  const Data = JSON.parse(localStorage.getItem("PasswordManager")) || [];
+
+  Data.push(DataPacket);
+
+  localStorage.setItem("PasswordManager", JSON.stringify(Data));
+
+  document.getElementById("siteName").value = " ";
+  document.getElementById("userName").value = " ";
+  document.getElementById("password").value = "";
+}
+
+function DownloadFile() {
+  const data = JSON.parse(localStorage.getItem("PasswordManager")) || [];
+
+  if (data.length <= 0) {
+    alert("No Data Found");
+    return;
+  }
+
+  const headers = Object.keys(data[0]).join(",") + "\n";
+  const rows = data.map((item) => Object.values(items).join(",")).join("\n");
+
+  const CSVContent=headers+rows;
+  const blob = new Blob([CSVContent],{type:"text/csv"});
+  const anchorTag = document.createElement("a");
+  
+  anchorTag.href=URL.createObjectURL(blob);
+  anchorTag.download = "data.csv";
+
+//   document.body.appendChild(anchorTag);
+  anchorTag.click();
+//   document.body.remove(anchorTag);
+
+localStorage.removeItem("PasswordManager");
+
+}
