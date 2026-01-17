788548aa7379740e3529ac042ee3aa53e561e231
Tanishtha Yadav <tanishthayadav12@gmail.com>
2026-01-17 20:28:37 +0530

    [Tanishtha] : Added Collapsing Sidebar

--- Cravings/client/src/components/userDashboard/UserSideBar.jsx
+++ Cravings/client/src/components/userDashboard/UserSideBar.jsx
@@ -6,76 +6,87 @@ import { TbTransactionRupee } from "react-icons/tb";
 import { SiHelpdesk } from "react-icons/si";
 import { GiHamburgerMenu } from "react-icons/gi";
 
-const UserSideBar = ({ active, setActive }) => {
+const UserSideBar = ({ active, setActive, collapse, setCollapse }) => {
   return (
-    <>
-      <div className="p-3">
-        <div
-          className="text-xl font-bold flex gap-1 items-center"
-          onClick={() => setCollapse(true)}
-        >
+    <div className="p-3 overflow-hidden transition-all duration-300">
+      {/* Header */}
+      <div className="flex items-center gap-2 mb-4">
+        <button onClick={() => setCollapse(!collapse)} className="p-2 text-xl cursor-pointer hover:scale-110">
           <GiHamburgerMenu />
-          User Dashboard
-        </div>
-        <hr />
-        <div className="grid gap-3 p-3">
-          <button
-            className={`flex gap-2 items-center p-3 rounded-xl ${
-              active === "overview"
-                ? "bg-(--color-secondary) text-(-color-text)"
-                : " hover:bg-gray-300"
-            }`}
-            onClick={() => setActive("overview")}
-          >
-            <MdOutlineAnalytics />
-            Overview
-          </button>
-          <button
-            className={`flex gap-2 items-center p-3 rounded-xl ${
-              active === "profile"
-                ? "bg-(--color-secondary) text-(-color-text)"
-                : " hover:bg-gray-300"
-            }`}
-            onClick={() => setActive("profile")}
-          >
-            <RiProfileFill />
-            Profile
-          </button>
-          <button
-            className={`flex gap-2 items-center p-3 rounded-xl ${
-              active === "orders"
-                ? "bg-(--color-secondary) text-(-color-text)"
-                : " hover:bg-gray-300"
-            }`}
-            onClick={() => setActive("orders")}
-          >
-            <RiShoppingBag3Line />
-            Orders
-          </button>
-          <button
-            className={`flex gap-2 items-center p-3 rounded-xl ${
-              active === "transaction"
-                ? "bg-(--color-secondary) text-(-color-text)"
-                : " hover:bg-gray-300"
-            }`}
-            onClick={() => setActive("transaction")}
-          >
-            <TbTransactionRupee />
-            Transactions
-          </button>
-          <button
-            className={`flex gap-2 items-center p-3 rounded-xl ${
-              active === "helpdesk"
-                ? "bg-(--color-secondary) text-(-color-text)"
-                : " hover:bg-gray-300"
-            }`}
-            onClick={() => setActive("helpdesk")}
-          >
-            <SiHelpdesk /> Help Desk
-          </button>
-        </div>
+        </button>
+
+        {!collapse && (
+          <span className="font-bold text-lg overflow-hidden text-nowrap">
+            User Dashboard
+          </span>
+        )}
       </div>
-    </>
+
+      <hr />
+
+      {/* Menu */}
+      <div className="grid gap-3 mt-4">
+        <button
+          className={`flex gap-3 items-center p-3 rounded-xl ${
+            active === "overview"
+              ? "bg-(--color-secondary) text-(-color-text)"
+              : "hover:bg-gray-300"
+          }`}
+          onClick={() => setActive("overview")}
+        >
+          <MdOutlineAnalytics className="text-xl" />
+          {!collapse && <span>Overview</span>}
+        </button>
+
+        <button
+          className={`flex gap-3 items-center p-3 rounded-xl ${
+            active === "profile"
+              ? "bg-(--color-secondary) text-(-color-text)"
+              : "hover:bg-gray-300"
+          }`}
+          onClick={() => setActive("profile")}
+        >
+          <RiProfileFill className="text-xl" />
+          {!collapse && <span>Profile</span>}
+        </button>
+
+        <button
+          className={`flex gap-3 items-center p-3 rounded-xl ${
+            active === "orders"
+              ? "bg-(--color-secondary) text-(-color-text)"
+              : "hover:bg-gray-300"
+          }`}
+          onClick={() => setActive("orders")}
+        >
+          <RiShoppingBag3Line className="text-xl" />
+          {!collapse && <span>Orders</span>}
+        </button>
+
+        <button
+          className={`flex gap-3 items-center p-3 rounded-xl ${
+            active === "transaction"
+              ? "bg-(--color-secondary) text-(-color-text)"
+              : "hover:bg-gray-300"
+          }`}
+          onClick={() => setActive("transaction")}
+        >
+          <TbTransactionRupee className="text-xl" />
+          {!collapse && <span>Transactions</span>}
+        </button>
+
+        <button
+          className={`flex gap-3 items-center p-3 rounded-xl ${
+            active === "helpdesk"
+              ? "bg-(--color-secondary) text-(-color-text)"
+              : "hover:bg-gray-300"
+          }`}
+          onClick={() => setActive("helpdesk")}
+        >
+          <SiHelpdesk className="text-xl" />
+          {!collapse && <span>Help Desk</span>}
+        </button>
+      </div>
+    </div>
   );
 };
 
--- Cravings/client/src/pages/dashboards/UserDashboard.jsx
+++ Cravings/client/src/pages/dashboards/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import UserSideBar from "../../components/userDashboard/UserSideBar";
-import UserOverview from "../../components/userDashboard/userOverview";
+import UserOverview from "../../components/userDashboard/UserOverview";
 import UserProfile from "../../components/userDashboard/UserProfile";
 import UserOrders from "../../components/userDashboard/UserOrders";
 import UserTransactions from "../../components/userDashboard/UserTransactions";
@@ -9,22 +9,33 @@ import UserHelpDesk from "../../components/userDashboard/UserHelpDesk";
 const UserDashboard = () => {
   const [active, setActive] = useState("overview");
   const [collapse, setCollapse] = useState(false);
+
   return (
-    <>
-      <div className="w-full h-[85vh] flex">
-        <div className="border border-green-500 bg-(--color-background) w-2/10">
-          <UserSideBar active={active} setActive={setActive} />
-        </div>
-        <div className="border border-red-500 w-8/10">
-          ContentBar
-          {active === "overview" && <UserOverview />}
-          {active === "profile" && <UserProfile />}
-          {active === "orders" && <UserOrders />}
-          {active === "transaction" && <UserTransactions />}
-          {active === "helpdesk" && <UserHelpDesk />}
-        </div>
+    <div className="w-full h-[85vh] flex">
+      <div
+        className={`border border-green-500 bg-(--color-background)
+        transition-all duration-300
+        ${collapse ? "w-[70]" : "w-2/10"}`}
+      >
+        <UserSideBar
+          active={active}
+          setActive={setActive}
+          collapse={collapse}
+          setCollapse={setCollapse}
+        />
       </div>
-    </>
+
+      <div
+        className={`border border-red-500 transition-all duration-300
+        ${collapse ? "w-[calc(100%-70px)]" : "w-8/10"}`}
+      >
+        {active === "overview" && <UserOverview />}
+        {active === "profile" && <UserProfile />}
+        {active === "orders" && <UserOrders />}
+        {active === "transaction" && <UserTransactions />}
+        {active === "helpdesk" && <UserHelpDesk />}
+      </div>
+    </div>
   );
 };
 
